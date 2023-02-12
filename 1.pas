21bc28de48dbb82a831a23b9b45dc98d493354e4
Кирилл Порубов <118163485+BalbesKiryusha43@users.noreply.github.com>
2023-02-12 14:53:59 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,24 @@
+﻿var 
+  x, y: text; 
+  z: integer; 
+  n: array of integer; 
+  i: integer; 
+begin
+  assign(x, '1.txt');
+  reset(x); 
+  while not eof(x) do 
+  begin
+    read(x, z);
+    setLength(n, length(n) + 1); 
+    n[high(n)] := z; 
+  end; 
+  close(x); 
+  assign(y, '2.txt');
+  rewrite(y); 
+  for i := high(n) downto 0 do 
+  begin
+    write(y, n[i]); 
+    writeln(y); 
+  end; 
+  close(y); 
+end.
